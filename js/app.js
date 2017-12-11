01cf6852ed99c0c2d415ac817ed41f7d8111df4e
Lauraisa <galindolaura.isabel@gmail.com>
2017-12-10 21:42:49 -0500

    Subiendo kitten

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/*var imgCat= document.getElementsByTagName('div')[0];
+var cat2 = document.getElementsByTagName('div')[1];
+var cat3 = document.getElementsByTagName('div')[2];
+function changeImg(){
+    
+    if(imgCat>='900px'){
+        return imgCat
+    }
+    
+    else if (cat2>='740px'){
+        return cat2
+    }
+      
+    else {
+        return cat3
+    } 
+
+}
